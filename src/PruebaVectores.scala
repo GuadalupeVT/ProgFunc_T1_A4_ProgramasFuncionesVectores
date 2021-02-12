9ca7b2b01bef95763b3414b5be381a3d0a4dafdd
Guadalupe Vazquez <guvadlt@gmail.com>
2021-02-12 13:29:00 -0600

    Se agrego la impresion del menu

--- src/PruebaVectores.scala
+++ src/PruebaVectores.scala
@@ -1,7 +1,38 @@
+import scala.io.StdIn._
 
 
 object PruebaVectores {
+  //Metodo para llenar el vector
+  def llenarVector(filas:Int, columnas:Int): Unit={
+    
+  }
+  
+  
+  
   def main(args: Array[String]): Unit = {
+    println("Ingrese el numero de filas del vector: ")
+    var filas=readInt()
+    println("Ingrese el numero de columnas del vector: ")
+    var columnas=readInt()
+    llenarVector(filas,columnas)
+    
+    var menu=10
+    while(menu>0){
+      //Impresion de menu
+     println("-----------------MENU-------------")
+      println("Selecciona una opcion...")
+      println("1) Imprimir vector")
+      println("2) Ordenar vector")
+      println("3) Calcular media")
+      println("4) Calcular moda")
+      println("5) Calcular mediana")
+      println("6) Desviación respecto de la media")
+      println("7) Desviación media")
+      println("8) Varianza")
+      println("9) Desviación estándar")
+      println("0) Salir")
+      menu=readInt()
+    }
     
   }
 }
